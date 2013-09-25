15b3ef3071c364fb50f496d3dbb4c971277edb4a
Jair Santos <jairdesouzasantos@gmail.com>
2013-09-25 09:46:59 -0300

    Commit das implementações da arvore binária

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -1,7 +1,27 @@
+require './lib/binary_tree/node.rb'
+
 module DataStructure
 
   class BinaryTree
-  
+
     attr_reader :root
+
+    def push(value)
+      insert @root, DataStructure::BinaryTree::Node.new(value)
+    end
+
+    private
+
+      def insert(current_node, new_node)
+        if (current_node == nil)
+          current_node = new_node
+        else
+          if (new_node.value < current_node.value)
+            insert(current_node.left, new_node)
+          else
+            insert(current_node.right, new_node)
+          end
+        end
+      end
   end  
 end
--- lib/binary_tree/node.rb
+++ lib/binary_tree/node.rb
@@ -1,6 +1,27 @@
 module DataStructure
 
-  class BinaryTree::Node
-    
+  class BinaryTree
+
+    class Node
+      
+      attr_reader :left, :right, :value
+
+      def initialize(value)
+        @value = value
+        @left = @right = nil
+      end
+
+      def left=(left)
+        @left = left
+      end
+
+      def right=(right)
+        @right = right
+      end
+
+      def value=(value)
+        @value = value
+      end
+    end
   end
 end
